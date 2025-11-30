291120c5aaf12627a0fe7cfd7388679160db0674
Vale <valeriavega270@gmail.com>
2025-11-29 21:23:33 -0600

    business- pre

--- PAWProject.Core/BusinessLog/SecretBusiness.cs
+++ PAWProject.Core/BusinessLog/SecretBusiness.cs
@@ -1,12 +1,56 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using PAWProject.Data.Repositories;
+using PAWProject.Models.Entities;
 
-namespace PAWProject.Core.NewFolder
-{
+namespace PAWProject.Core.NewFolder;
     public interface ISecretBusiness
     {
+        /// <summary>
+        /// Deletes the secret associated with the id.
+        /// </summary>
+        /// <param name="id">The secret id.</param>
+        /// <returns>True if deletion was successful, false otherwise.</returns>
+        Task<bool> DeleteSecretAsync(int id);
+
+        /// <summary>
+        /// Gets secrets. If id is provided, returns only that secret; otherwise returns all secrets.
+        /// </summary>
+        /// <param name="id">Optional secret id.</param>
+        /// <returns>A collection of secrets.</returns>
+        Task<IEnumerable<Secret>> GetSecrets(int? id);
+
+        /// <summary>
+        /// Saves a secret (creates or updates).
+        /// </summary>
+        /// <param name="secret">The secret to save.</param>
+        /// <returns>True if save was successful, false otherwise.</returns>
+        Task<bool> SaveSecretAsync(Secret secret);
     }
-}
+
+    public class SecretBusiness(IRepositorySecret repositorySecret) : ISecretBusiness
+    {
+        /// <inheritdoc />
+        public async Task<bool> SaveSecretAsync(Secret secret)
+        {
+            return await repositorySecret.UpdateAsync(secret);
+        }
+
+        /// <inheritdoc />
+        public async Task<bool> DeleteSecretAsync(int id)
+        {
+            var secret = await repositorySecret.FindAsync(id);
+            if (secret is null)
+                return false;
+
+            return await repositorySecret.DeleteAsync(secret);
+        }
+
+        /// <inheritdoc />
+        public async Task<IEnumerable<Secret>> GetSecrets(int? id)
+        {
+            if (id is null)
+                return await repositorySecret.ReadAsync();
+
+            var secret = await repositorySecret.FindAsync(id.Value);
+            return secret is null ? [] : new[] { secret };
+        }
+    }
--- PAWProject.Core/BusinessLog/SourceItemBusiness.cs
+++ PAWProject.Core/BusinessLog/SourceItemBusiness.cs
@@ -1,12 +1,57 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using PAWProject.Data.Repositories;
+using PAWProject.Models.Entities;
 
-namespace PAWProject.Core.NewFolder
+namespace PAWProject.Core.NewFolder;
+
+    public interface ISourceItemBusiness
 {
-    public interface ISecretmBusiness
+    /// <summary>
+    /// Deletes the source item associated with the id.
+    /// </summary>
+    /// <param name="id">The source item id.</param>
+    /// <returns>True if deletion was successful, false otherwise.</returns>
+    Task<bool> DeleteSourceItemAsync(int id);
+
+    /// <summary>
+    /// Gets source items. If id is provided, returns only that item; otherwise returns all items.
+    /// </summary>
+    /// <param name="id">Optional source item id.</param>
+    /// <returns>A collection of source items.</returns>
+    Task<IEnumerable<SourceItem>> GetSourceItems(int? id);
+
+    /// <summary>
+    /// Saves a source item (creates or updates).
+    /// </summary>
+    /// <param name="item">The source item to save.</param>
+    /// <returns>True if save was successful, false otherwise.</returns>
+    Task<bool> SaveSourceItemAsync(SourceItem item);
+}
+
+public class SourceItemBusiness(IRepositorySourceItem repositorySourceItem) : ISourceItemBusiness
+{
+    /// <inheritdoc />
+    public async Task<bool> SaveSourceItemAsync(SourceItem item)
     {
+        return await repositorySourceItem.UpdateAsync(item);
+    }
+
+    /// <inheritdoc />
+    public async Task<bool> DeleteSourceItemAsync(int id)
+    {
+        var item = await repositorySourceItem.FindAsync(id);
+        if (item is null)
+            return false;
+
+        return await repositorySourceItem.DeleteAsync(item);
+    }
+
+    /// <inheritdoc />
+    public async Task<IEnumerable<SourceItem>> GetSourceItems(int? id)
+    {
+        if (id is null)
+            return await repositorySourceItem.ReadAsync();
+
+        var item = await repositorySourceItem.FindAsync(id.Value);
+        return item is null ? [] : new[] { item };
     }
 }
