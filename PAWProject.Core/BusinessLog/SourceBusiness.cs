094d6ecb10608736096bcd40cbd8ca41b63ae811
andreyjpg <andreyjpg61@gmail.com>
2025-12-04 14:41:30 -0600

    Business y repositorios (#4)
    * +DATA/CORE
    +MSSQL > Scrip SQL
    
    Arquitectura de Business por implementar
    
    * business- pre
    
    ---------
    
    Co-authored-by: Vale <valeriavega270@gmail.com>

---
+++ PAWProject.Core/BusinessLog/SourceBusiness.cs
@@ -0,0 +1,52 @@
+﻿using PAWProject.Data.Repositories;
+using PAWProject.Models.Entities;
+
+namespace PAWProject.Core.NewFolder;
+    public interface ISourceBusiness
+    {
+        /// <summary>
+        /// Deletes the source associated with the id.
+        /// </summary>
+        /// <param name="id">The source id.</param>
+        /// <returns>True if deletion was successful, false otherwise.</returns>
+        Task<bool> DeleteSourceAsync(int id);
+        /// <summary>
+        /// Gets sources. If id is provided, returns only that source; otherwise returns all sources.
+        /// </summary>
+        /// <param name="id">Optional source id.</param>
+        /// <returns>A collection of sources.</returns>
+        Task<IEnumerable<Source>> GetSources(int? id);
+        /// <summary>
+        /// Saves a source (creates or updates).
+        /// </summary>
+        /// <param name="source">The source to save.</param>
+        /// <returns>True if save was successful, false otherwise.</returns>
+        Task<bool> SaveSourceAsync(Source source);
+    }
+    public class SourceBusiness(IRepositorySource repositorySource) : ISourceBusiness
+    {
+        /// <inheritdoc />
+        public async Task<bool> SaveSourceAsync(Source source)
+        {
+            return await repositorySource.UpdateAsync(source);
+        }
+        /// <inheritdoc />
+        public async Task<bool> DeleteSourceAsync(int id)
+        {
+            var source = await repositorySource.FindAsync(id);
+            if (source is null)
+                return false;
+
+            return await repositorySource.DeleteAsync(source);
+        }
+
+        /// <inheritdoc />
+        public async Task<IEnumerable<Source>> GetSources(int? id)
+        {
+            if (id is null)
+                return await repositorySource.ReadAsync();
+
+            var source = await repositorySource.FindAsync(id.Value);
+            return source is null ? [] : new[] { source };
+        }
+    }
---
+++ PAWProject.Data/Repositories/RepositorySecret.cs
@@ -0,0 +1,24 @@
+﻿using PAWProject.Models.Entities;
+using PAWProject.Data.MSSQL;
+
+namespace PAWProject.Data.Repositories;
+
+public interface IRepositorySecret
+
+{
+    Task<bool> UpsertAsync(Secret entity, bool isUpdating);
+    Task<bool> CreateAsync(Secret entity);
+    Task<bool> DeleteAsync(Secret entity);
+    Task<IEnumerable<Secret>> ReadAsync();
+    Task<Secret> FindAsync(int id);
+    Task<bool> UpdateAsync(Secret entity);
+    Task<bool> UpdateManyAsync(IEnumerable<Secret> entities);
+    Task<bool> ExistsAsync(Secret entity);
+}
+
+public class RepositorySecret : RepositoryBase<Secret>, IRepositorySecret
+{
+    public RepositorySecret(NewsHubContext context) : base(context)
+    {
+    }
+}
